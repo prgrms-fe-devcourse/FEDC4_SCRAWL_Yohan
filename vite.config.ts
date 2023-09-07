f1a3f78d98ea80ff3ca7cf34ba05f2de46293fb7
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-07 14:29:04 +0900

    chore: vite.config 절대경로 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@apis", replacement: "/src/apis" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@store", replacement: "/src/store" },
+      { find: "@type", replacement: "/src/types" },
+      { find: "@constants", replacement: "/src/constants" },
+      { find: "@utils", replacement: "/src/utils" }
+    ]
+  }
 });
