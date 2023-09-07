9525ff6daef49407f585e61477378993eb2012d2
AhnHyesu <linim@naver.com>
2023-09-07 17:39:48 +0900

    feat: Text 컴포넌트 작성

---
+++ src/components/atoms/Text.tsx
@@ -0,0 +1,45 @@
+import { css } from "@emotion/react";
+
+type TextProps = {
+  children: React.ReactNode;
+  block?: boolean;
+  paragraph?: boolean;
+  size: number;
+  strong?: boolean;
+  underline?: boolean;
+  delete?: boolean;
+  color?: string;
+};
+
+const Text = ({
+  children,
+  block,
+  paragraph,
+  size,
+  strong,
+  underline,
+  delete: del,
+  color,
+  ...props
+}: TextProps) => {
+  const Tag = block ? "div" : paragraph ? "p" : "span";
+  if (del) {
+    children = <del>{children}</del>;
+    console.log(children);
+  }
+
+  return (
+    <Tag
+      css={css`
+        font-weight: ${strong ? "bold" : undefined};
+        font-size: ${size}px;
+        text-decoration: ${underline ? "underline" : undefined};
+        color: ${color};
+      `}
+      {...props}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Text;
