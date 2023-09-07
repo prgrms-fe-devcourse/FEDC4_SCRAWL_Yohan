4c5867e183cc7b05673abe846de5ff9cab91decf
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-07 18:33:36 +0900

    feat: api 상수 작성

---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://kdt.frontend.4th.programmers.co.kr:5003";
+
+export const NETWORK = {
+  TIMEOUT: 10000
+} as const;
+
+export const ACCESS_TOKEN_KEY = "ACCESS_TOKEN";
