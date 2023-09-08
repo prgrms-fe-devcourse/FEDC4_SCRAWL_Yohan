3b06353ac275fff58707e1a6eb5a579d2cc313b2
AhnHyesu <linim@naver.com>
2023-09-08 18:31:17 +0900

    feat: global css setting font & reset 설정

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  @import "/path/to/reset-css/reset.css";
+  @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@200;300;400;700&display=swap");
+  body {
+    font-family: "IBM Plex Sans KR", sans-serif;
+  }
+`;
