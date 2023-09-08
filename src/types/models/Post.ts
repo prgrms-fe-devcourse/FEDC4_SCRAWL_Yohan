1b4dd19101d02146636dda608772f3b81d1ca3a9
doggopawer <doggopawer@gmail.com>
2023-09-08 17:33:41 +0900

    데이터 모델 타입 정의

---
+++ src/types/models/Post.ts
@@ -0,0 +1,16 @@
+import { Channel } from "./Channel";
+import { Like } from "./Like";
+import { User } from "./User";
+
+export interface Post {
+  likes: Like[];
+  comments: Comment[];
+  _id: string;
+  image?: string;
+  imagePublicId?: string;
+  title: string;
+  channel: Channel;
+  author: User;
+  createdAt: string;
+  updatedAt: string;
+}
