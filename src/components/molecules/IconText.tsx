7a8038412a5e6b94e15942e81d4183d938ce35cf
AhnHyesu <linim@naver.com>
2023-09-09 21:05:22 +0900

    feat: IconText molecule 완성

---
+++ src/components/molecules/IconText.tsx
@@ -0,0 +1,49 @@
+import Icon from "@components/atoms/Icon";
+import { IconProps } from "@components/atoms/Icon";
+import Text from "@components/atoms/Text";
+import { TextProps } from "@components/atoms/Text";
+
+const IconText = ({
+  iconValue,
+  textValue
+}: {
+  iconValue: IconProps;
+  textValue: TextProps;
+}) => {
+  const {
+    Svg,
+    size: iconSize = 20,
+    fill = "#8B8B8B",
+    ...iconProps
+  } = iconValue;
+  const {
+    children,
+    block,
+    paragraph,
+    size,
+    strong,
+    underline,
+    delete: del,
+    color,
+    ...textprops
+  } = textValue;
+
+  return (
+    <div>
+      <Icon Svg={Svg} size={iconSize} fill={fill} {...iconProps} />
+      <Text
+        children={children}
+        block={block}
+        paragraph={paragraph}
+        size={size}
+        strong={strong}
+        underline={underline}
+        delete={del}
+        color={color}
+        {...textprops}
+      />
+    </div>
+  );
+};
+
+export default IconText;
