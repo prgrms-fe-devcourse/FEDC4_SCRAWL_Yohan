1d071ce60195a1db6f0bfa933ead42eb4f1c6baf
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-11 18:53:40 +0900

    feat: ChannelPage 작성 - ChannelPageSkeleton 도 같이 구현

---
+++ src/pages/ChannelPage/ChannelPage.styles.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const channelPageOuterStyle = css`
+  margin: 20px 0;
+`;
+
+export const channelPageHeaderStyle = css`
+  gap: 10px;
+`;
+
+export const cardListStyle = css`
+  margin-top: 30px;
+  flex-wrap: wrap;
+`;
