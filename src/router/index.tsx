3f8e3ab55b5ce9f681ef5e891c68226b28c22962
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-11 14:22:20 +0900

    feat: PageTemplate 구현 및 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { PageTemplate } from "@components/templates/PageTemplate";
+
 import { PATH } from "@constants/index";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PATH.HOME} element={<div>home</div>} />
-        <Route path={PATH.CHANNEL(":channelId")} element={<div>channel</div>} />
-        <Route path={PATH.ARTICLE(":articleId")} element={<div>article</div>} />
-        <Route
-          path={PATH.EDIT_ARTICLE(":articleId")}
-          element={<div>article edit</div>}
-        />
-        <Route
-          path={PATH.QUESTION(":questionId")}
-          element={<div>question</div>}
-        />
-        <Route
-          path={PATH.EDIT_QUESTION(":questionId")}
-          element={<div>question edit</div>}
-        />
-        <Route path={PATH.SEARCH} element={<div>search</div>} />
-        <Route path={PATH.USER(":userId")} element={<div>user info</div>} />
+        <Route path="" element={<PageTemplate />}>
+          <Route path={PATH.HOME} element={<div>home</div>} />
+          <Route
+            path={PATH.CHANNEL(":channelId")}
+            element={<div>channel</div>}
+          />
+          <Route
+            path={PATH.ARTICLE(":articleId")}
+            element={<div>article</div>}
+          />
+          <Route
+            path={PATH.EDIT_ARTICLE(":articleId")}
+            element={<div>article edit</div>}
+          />
+          <Route
+            path={PATH.QUESTION(":questionId")}
+            element={<div>question</div>}
+          />
+          <Route
+            path={PATH.EDIT_QUESTION(":questionId")}
+            element={<div>question edit</div>}
+          />
+          <Route path={PATH.SEARCH} element={<div>search</div>} />
+          <Route path={PATH.USER(":userId")} element={<div>user info</div>} />
+        </Route>
         <Route path={PATH.SIGNUP} element={<div>signup</div>} />
         <Route path={PATH.LOGIN} element={<div>login</div>} />
         <Route path={PATH.PASSWORD} element={<div>password</div>} />
