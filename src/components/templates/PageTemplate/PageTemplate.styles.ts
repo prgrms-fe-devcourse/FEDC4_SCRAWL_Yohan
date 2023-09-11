9ce46359992696aab3faa0dd3a57568488081f3a
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-11 18:52:25 +0900

    refactor: PageTemplate 사이드바 fixed 로 변경

--- src/components/templates/PageTemplate/PageTemplate.styles.ts
+++ src/components/templates/PageTemplate/PageTemplate.styles.ts
@@ -1,6 +1,10 @@
 import { css } from "@emotion/react";
 
 export const pageTemplateWrapperStyle = css`
-  gap: 20px;
   position: relative;
+  overflow-x: hidden;
+`;
+
+export const pageInnerWrapperStyle = css`
+  margin-left: 330px;
 `;
