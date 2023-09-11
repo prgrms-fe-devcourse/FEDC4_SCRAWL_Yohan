c1192b6064f89fabf96efce796d4bd5a212b602b
AhnHyesu <linim@naver.com>
2023-09-12 01:20:06 +0900

    fix: ChannelSelect 컴포넌트 스타일 변경

--- src/components/organisms/ArticleWrite/ChannelSelect.tsx
+++ src/components/organisms/ArticleWrite/ChannelSelect.tsx
@@ -7,6 +7,8 @@ import Icon from "@components/atoms/Icon";
 
 import useDetectClose from "@hooks/useDetectClose";
 
+import { useThemeStore } from "@stores/theme.store";
+
 import { BracketLower, BracketUpper } from "@assets/svg";
 
 type ChannelProps = {
@@ -33,8 +35,52 @@ const ChannelSelect = () => {
   const dropdownRef = useRef(null);
   const [channelIdentify, setChannelIdentify] = useState("채널 선택");
   const channelList = ["프론트엔드", "백엔드", "AI"];
+  const { theme } = useThemeStore();
 
   const [isOpen, setIsOpen] = useDetectClose(dropdownRef, false);
+
+  const CSSdropdown = css`
+    position: relative;
+    input {
+      font-size: 20px;
+      color: ${theme.PRIMARY};
+      font-weight: 400;
+      min-width: 100px;
+      border: 2px;
+      background-color: transparent;
+      :hover {
+        cursor: pointer;
+      }
+    }
+    .selectInput {
+      background-color: ${theme.BACKGROUND200};
+      padding: 5px;
+      border-radius: 5px;
+    }
+    ul {
+      position: absolute;
+      top: 100%;
+      margin-top: 5px;
+      color: ${theme.TEXT600};
+      background-color: ${theme.BACKGROUND100};
+      border-radius: 5px;
+      list-style: none;
+      z-index: 1;
+      :hover {
+        cursor: pointer;
+      }
+    }
+    li {
+      margin: 2.5px 5px 2.5px 5px;
+      padding: 5px;
+      border-radius: 5px;
+      min-width: 100px;
+      :hover {
+        background-color: ${theme.BACKGROUND300};
+      }
+    }
+  `;
+
   return (
     <>
       <div css={CSSdropdown} ref={dropdownRef}>
@@ -68,34 +114,3 @@ const ChannelSelect = () => {
   );
 };
 export default ChannelSelect;
-
-const CSSdropdown = css`
-  input {
-    font-size: 20px;
-    color: blue;
-    font-weight: 400;
-    min-width: 50px;
-    border: 2px;
-    background-color: transparent;
-  }
-  .selectInput {
-    background-color: #d1d1d1;
-    padding: 5px;
-    border-radius: 5px;
-  }
-  ul {
-    margin-top: 5px;
-    color: rgb(37, 37, 37);
-    background-color: #e4e4e4;
-    border-radius: 5px;
-  }
-  li {
-    margin: 2.5px 5px 2.5px 5px;
-    padding: 5px;
-    border-radius: 5px;
-    z-index: 1;
-  }
-  li:hover {
-    background-color: #929292;
-  }
-`;
