c897d3ab0e0fdc0569af1415cbc0f84ef09c0b2b
AhnHyesu <linim@naver.com>
2023-09-12 01:27:32 +0900

    fix: ArticleTag, ArticleTitle 스타일 변경

--- src/components/organisms/ArticleWrite/ArticleTag.tsx
+++ src/components/organisms/ArticleWrite/ArticleTag.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+import { css } from "@emotion/react";
+
 import Input from "@components/atoms/Input";
+
+import { useThemeStore } from "@stores/theme.store";
 
 interface ArticleTagProps {
   stateChange: (value: string) => void;
@@ -8,7 +12,7 @@ interface ArticleTagProps {
 }
 const ArticleTag = ({ stateChange, width }: ArticleTagProps) => {
   const [inputValue, setInputValue] = useState("");
-
+  const { theme } = useThemeStore();
   useEffect(() => {
     stateChange(inputValue);
   });
@@ -19,9 +23,14 @@ const ArticleTag = ({ stateChange, width }: ArticleTagProps) => {
       height="30px"
       width={width}
       fontSize="20px"
+      color={theme.TEXT600}
+      background={theme.BACKGROUND100}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
         setInputValue(e.target.value)
       }
+      css={css`
+        border: none;
+      `}
     />
   );
 };
--- src/components/organisms/ArticleWrite/ArticleTitle.tsx
+++ src/components/organisms/ArticleWrite/ArticleTitle.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+import { css } from "@emotion/react";
+
 import Input from "@components/atoms/Input";
+
+import { useThemeStore } from "@stores/theme.store";
 
 interface ArticleTagProps {
   stateChange: (value: string) => void;
@@ -8,7 +12,7 @@ interface ArticleTagProps {
 }
 const ArticleTitle = ({ stateChange, width }: ArticleTagProps) => {
   const [inputValue, setInputValue] = useState("");
-
+  const { theme } = useThemeStore();
   useEffect(() => {
     stateChange(inputValue);
   });
@@ -19,9 +23,14 @@ const ArticleTitle = ({ stateChange, width }: ArticleTagProps) => {
       height="70px"
       width={width}
       fontSize="35px"
+      color={theme.TEXT600}
+      background={theme.BACKGROUND100}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
         setInputValue(e.target.value)
       }
+      css={css`
+        border: none;
+      `}
     />
   );
 };
