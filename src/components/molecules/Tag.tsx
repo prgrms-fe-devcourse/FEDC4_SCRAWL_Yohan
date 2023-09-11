5caaa62a6a778ef8c9fd2f8f40f7be739e7aa77f
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-11 14:47:18 +0900

    refactor: Tag, IconText 컴포넌트가 ,,,props를 받게 수정

--- src/components/molecules/Tag.tsx
+++ src/components/molecules/Tag.tsx
@@ -1,18 +1,25 @@
+import { HTMLAttributes } from "react";
+
 import Text from "@components/atoms/Text";
 
 import { useThemeStore } from "@stores/theme.store";
 
-type TagProps = {
-  size: number;
-  name: string;
-};
+import { Combine } from "@type/Combine";
 
-const Tag = ({ size = 12, name }: TagProps) => {
+type TagProps = Combine<
+  {
+    size: number;
+    name: string;
+  },
+  HTMLAttributes<HTMLElement>
+>;
+
+const Tag = ({ size = 12, name, ...props }: TagProps) => {
   const { theme } = useThemeStore();
   const tag = name.replace(/__/g, "");
 
   return (
-    <Text size={size} color={theme.PRIMARY}>
+    <Text size={size} color={theme.PRIMARY} {...props}>
       #{tag}
     </Text>
   );
