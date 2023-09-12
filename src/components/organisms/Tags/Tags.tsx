b67c863d57b443cf3fb0762fd679dbe333bfa99c
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-12 22:07:20 +0900

    rename: Tag 컴포넌트 organism으로 이동

--- src/components/organisms/Tags/Tags.tsx
+++ src/components/organisms/Tags/Tags.tsx
@@ -1,21 +1,33 @@
+import { HTMLAttributes } from "react";
 import { createSearchParams, useNavigate } from "react-router-dom";
 
 import Flex from "@components/atoms/Flex";
 import Tag from "@components/molecules/Tag";
 
+import { Combine } from "@type/Combine";
+
 import { PATH } from "@constants/index";
 
 import { tagStyle, tagsContainerStyle } from "./Tags.styles";
 
-const Tags = ({ tags }: { tags: string[] }) => {
+type TagsProps = Combine<
+  {
+    tags: string[];
+    gap?: number;
+    size?: number;
+  },
+  HTMLAttributes<HTMLDivElement>
+>;
+
+const Tags = ({ tags, gap = 5, size = 12, ...props }: TagsProps) => {
   const navigate = useNavigate();
 
   return (
-    <Flex gap={5} css={tagsContainerStyle}>
+    <Flex gap={gap} css={tagsContainerStyle} {...props}>
       {tags.map((tag, i) => (
         <Tag
           key={`${tag}${i}`}
-          size={12}
+          size={size}
           name={tag}
           css={tagStyle}
           onClick={() =>
