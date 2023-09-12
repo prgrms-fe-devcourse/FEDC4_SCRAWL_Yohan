95d5a4754096a3a5b3cae4b36951ff5bfe5b17a8
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-12 16:03:03 +0900

    refactor: PageTemplate 스타일 수정

--- src/components/templates/PageTemplate/PageTemplate.styles.ts
+++ src/components/templates/PageTemplate/PageTemplate.styles.ts
@@ -2,9 +2,11 @@ import { css } from "@emotion/react";
 
 export const pageTemplateWrapperStyle = css`
   position: relative;
+  height: 100vh;
   overflow-x: hidden;
 `;
 
 export const pageInnerWrapperStyle = css`
+  flex-grow: 1;
   margin-left: 330px;
 `;
