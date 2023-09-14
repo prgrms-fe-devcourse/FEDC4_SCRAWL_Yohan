2c4590582cc93a21841595f0186f3843233ea76d
AhnHyesu <linim@naver.com>
2023-09-14 18:24:58 +0900

    feat: 태그 엔터 입력시 컴포넌트로 추가 완료

--- src/components/organisms/ArticleWrite/ArticleTag.tsx
+++ src/components/organisms/ArticleWrite/ArticleTag.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 
 import { css } from "@emotion/react";
 
+import Flex from "@components/atoms/Flex";
 import Input from "@components/atoms/Input";
+import Tag from "@components/molecules/Tag";
 
 import { useThemeStore } from "@stores/theme.store";
 
@@ -12,26 +14,56 @@ interface ArticleTagProps {
 }
 const ArticleTag = ({ stateChange, width }: ArticleTagProps) => {
   const [inputValue, setInputValue] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
   const { theme } = useThemeStore();
   useEffect(() => {
     stateChange(inputValue);
+    console.log(tags);
   });
   return (
-    <Input
-      value={inputValue}
-      placeholder="태그를 입력하세요"
-      height="30px"
-      width={width}
-      fontSize="20px"
-      color={theme.TEXT600}
-      background={theme.BACKGROUND100}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        setInputValue(e.target.value)
-      }
-      css={css`
-        border: none;
-      `}
-    />
+    <>
+      <Input
+        value={inputValue}
+        placeholder="태그를 입력하세요"
+        height="30px"
+        width={width}
+        fontSize="20px"
+        color={theme.TEXT600}
+        background={theme.BACKGROUND100}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setInputValue(e.target.value)
+        }
+        onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          console.log(e);
+          if (e.nativeEvent.isComposing || !inputValue) return;
+          if (e.key === "Enter") {
+            setTags([...tags, `__${inputValue}__`]);
+            setInputValue("");
+          }
+        }}
+        css={css`
+          border: none;
+        `}
+      />
+      <Flex
+        gap={10}
+        css={css`
+          padding: 10px;
+        `}>
+        {tags.map((tag, index) => {
+          return (
+            <Tag
+              key={index}
+              size={16}
+              name={tag}
+              onClick={() => {
+                setTags([...tags].filter((x) => x !== tag));
+              }}
+            />
+          );
+        })}
+      </Flex>
+    </>
   );
 };
 export default ArticleTag;
