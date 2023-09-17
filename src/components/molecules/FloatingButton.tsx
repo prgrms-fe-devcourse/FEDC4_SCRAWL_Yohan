42f7a524900f33f572ad8571e2fee62eb80bdd61
AhnHyesu <linim@naver.com>
2023-09-18 00:53:02 +0900

    fix: props 받을 수 있도록 수정 & 그림자 추가

--- src/components/molecules/FloatingButton.tsx
+++ src/components/molecules/FloatingButton.tsx
@@ -1,16 +1,23 @@
+import { HTMLAttributes } from "react";
+
+import { css } from "@emotion/react";
+
 import Button from "@components/atoms/Button";
 import { ButtonProps } from "@components/atoms/Button";
 import Flex from "@components/atoms/Flex";
 import Icon from "@components/atoms/Icon";
 import { IconProps } from "@components/atoms/Icon";
 
+import { useThemeStore } from "@stores/theme.store";
+
 const FloatingButton = ({
   iconValue,
-  buttonValue
+  buttonValue,
+  ...props
 }: {
   iconValue: IconProps;
   buttonValue: ButtonProps;
-}) => {
+} & HTMLAttributes<HTMLDivElement>) => {
   const {
     Svg,
     size: iconSize = 20,
@@ -23,24 +30,32 @@ const FloatingButton = ({
     fontSize = "14px",
     background = "#ffffff",
     color = "#FFF",
-    borderRadius = "50px"
+    borderRadius = "50px",
+    ...buttonProps
   } = buttonValue;
+  const { theme } = useThemeStore();
   function click() {
     return;
   }
   return (
-    <Button
-      width={width}
-      height={height}
-      fontSize={fontSize}
-      background={background}
-      color={color}
-      borderRadius={borderRadius}
-      onClick={click}>
-      <Flex justify="center">
-        <Icon Svg={Svg} size={iconSize} fill={fill} {...iconProps} />
-      </Flex>
-    </Button>
+    <div {...props}>
+      <Button
+        width={width}
+        height={height}
+        fontSize={fontSize}
+        background={background}
+        color={color}
+        borderRadius={borderRadius}
+        onClick={click}
+        css={css`
+          box-shadow: ${theme.SHADOW};
+        `}
+        {...buttonProps}>
+        <Flex justify="center">
+          <Icon Svg={Svg} size={iconSize} fill={fill} {...iconProps} />
+        </Flex>
+      </Button>
+    </div>
   );
 };
 
