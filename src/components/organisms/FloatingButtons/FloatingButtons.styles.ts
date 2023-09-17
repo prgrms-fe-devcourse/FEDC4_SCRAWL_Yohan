6c1db69b2a02167818991495780bad0cab2c7d9b
AhnHyesu <linim@naver.com>
2023-09-18 01:39:40 +0900

    feat: FloatingButtons 추가 1. 위치 fixed로 어떤 위치에서던 동일하게 보이게 함 2. /create 경로에선 나타나지 않도록 설정 3. 로그아웃시 글 작성 버튼은 나타나지 않게 설정

---
+++ src/components/organisms/FloatingButtons/FloatingButtons.styles.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+export const floatingButtonOuterStyleHide = css`
+  display: none;
+`;
+
+export const floatingButtonLocation = css`
+  position: fixed;
+  right: 150px;
+  bottom: 25px;
+  :hover {
+    cursor: pointer;
+  }
+`;
