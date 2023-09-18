e0cab868fd685be79e22018cd0b44af93bec3dcb
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-18 16:59:53 +0900

    chore: Login 컴포넌트 사용하지 않는다는 주석 작성

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,6 +6,7 @@ import { useUserByTokenQuery } from "@hooks/api/useUserByTokenQuery";
 
 import { useTokenStore } from "@stores/token.store";
 
+/** 현재 사용되지 않는 컴포넌트 입니다. */
 const Login = () => {
   const setAccessToken = useTokenStore((state) => state.setAccessToken);
   const queryClient = useQueryClient();
