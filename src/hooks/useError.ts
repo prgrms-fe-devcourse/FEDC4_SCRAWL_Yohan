1e505f6b3f20fd4841a64e21eb7433561b8374e4
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-18 14:15:22 +0900

    refactor: useError 훅 수정 - useCallback 추가 - 리턴함수 이름 변경

--- src/hooks/useError.ts
+++ src/hooks/useError.ts
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useError = () => {
   const [, set] = useState();
 
-  const throwLazyError = (error: Error) => {
+  const dispatchError = useCallback((error: Error) => {
     set(() => {
       throw error;
     });
-  };
+  }, []);
 
-  return { throwLazyError };
+  return { dispatchError };
 };
