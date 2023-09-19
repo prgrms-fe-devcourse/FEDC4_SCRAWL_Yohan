7398a78b706db25436e634c45540a8ab278c8f78
AhnHyesu <linim@naver.com>
2023-09-19 21:05:26 +0900

    fix: Sidebar 불러오는 방식 변경

--- src/components/templates/PageTemplate/PageTemplate.tsx
+++ src/components/templates/PageTemplate/PageTemplate.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import Flex from "@components/atoms/Flex";
 import FloatingButtons from "@components/organisms/FloatingButtons/FloatingButtons";
-import Sidebar from "@components/organisms/Sidebar";
+import { Sidebar } from "@components/organisms/Sidebar";
 import {
   pageInnerWrapperStyle,
   pageTemplateWrapperStyle
