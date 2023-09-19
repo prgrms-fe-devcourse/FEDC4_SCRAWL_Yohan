b88bf3074fb5d400e60891031a1b990b96fb8c41
AhnHyesu <linim@naver.com>
2023-09-19 22:38:13 +0900

    fix: ArticleChannelSelect 컴포넌트 스타일 분리 & z-index 수정

--- src/components/organisms/ArticleWrite/ArticleChannelSelect.tsx
+++ src/components/organisms/ArticleWrite/ArticleChannelSelect.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 
-import { css } from "@emotion/react";
-
 import Flex from "@components/atoms/Flex";
 import Icon from "@components/atoms/Icon";
 
@@ -11,6 +9,8 @@ import useDetectClose from "@hooks/useDetectClose";
 import { useThemeStore } from "@stores/theme.store";
 
 import { ExpandLess, ExpandMore } from "@assets/svg";
+
+import { articleChanneldropdown } from "./ArticleWrite.styles";
 
 type ChannelProps = {
   value: string;
@@ -41,88 +41,40 @@ const ArticleChannelSelect = ({ stateChange }: ArticleChannelSelectProps) => {
   const { theme } = useThemeStore();
   const channelList = [...useChannelsQuery().channels];
   const [isOpen, setIsOpen] = useDetectClose(dropdownRef, false);
-  console.log("channel:", channelList);
   const selectedChannel = channelList.find((x) => x.name === channelIdentify);
-
-  const CSSdropdown = css`
-    position: relative;
-    input {
-      font-size: 20px;
-      color: ${theme.PRIMARY};
-      font-weight: 400;
-      min-width: 100px;
-      border: 2px;
-      background-color: transparent;
-      :hover {
-        cursor: pointer;
-      }
-    }
-    .selectInput {
-      background-color: ${theme.BACKGROUND200};
-      padding: 5px;
-      border-radius: 5px;
-    }
-    ul {
-      position: absolute;
-      top: 100%;
-      margin-top: 5px;
-      padding: 0;
-      color: ${theme.TEXT600};
-      background-color: ${theme.BACKGROUND100};
-      border-radius: 5px;
-      border: 1px solid var(--border-color);
-      list-style: none;
-      z-index: 1;
-      :hover {
-        cursor: pointer;
-      }
-    }
-    li {
-      margin: 2.5px 5px;
-      padding: 5px;
-      border-radius: 5px;
-      min-width: 100px;
-      text-align: center;
-      :hover {
-        background-color: ${theme.BACKGROUND300};
-      }
-    }
-  `;
 
   useEffect(() => {
     stateChange(selectedChannel ? selectedChannel._id : "");
   });
 
   return (
-    <>
-      <div css={CSSdropdown} ref={dropdownRef}>
-        <Flex direction="column">
-          <div className="selectInput" onClick={() => setIsOpen(!isOpen)}>
-            <Flex align="center">
-              <input type="button" value={channelIdentify} />
-              {isOpen ? (
-                <Icon Svg={ExpandLess} size={15} />
-              ) : (
-                <Icon Svg={ExpandMore} size={15} />
-              )}
-            </Flex>
-          </div>
-          {isOpen && (
-            <ul>
-              {channelList.map(({ name, _id }) => (
-                <Select
-                  key={_id}
-                  value={name}
-                  setIsOpen={setIsOpen}
-                  setChannelIdentify={setChannelIdentify}
-                  isOpen={isOpen}
-                />
-              ))}
-            </ul>
-          )}
-        </Flex>
-      </div>
-    </>
+    <div css={articleChanneldropdown(theme)} ref={dropdownRef}>
+      <Flex direction="column">
+        <div className="selectInput" onClick={() => setIsOpen(!isOpen)}>
+          <Flex align="center">
+            <input type="button" value={channelIdentify} />
+            {isOpen ? (
+              <Icon Svg={ExpandLess} size={15} />
+            ) : (
+              <Icon Svg={ExpandMore} size={15} />
+            )}
+          </Flex>
+        </div>
+        {isOpen && (
+          <ul>
+            {channelList.map(({ name, _id }) => (
+              <Select
+                key={_id}
+                value={name}
+                setIsOpen={setIsOpen}
+                setChannelIdentify={setChannelIdentify}
+                isOpen={isOpen}
+              />
+            ))}
+          </ul>
+        )}
+      </Flex>
+    </div>
   );
 };
 export default ArticleChannelSelect;
