5b19d0b7db8c53b217cf8822d2916e85fb8cd93a
AhnHyesu <linim@naver.com>
2023-09-20 16:48:41 +0900

    feat: article update api 추가

---
+++ src/apis/article/updateArticle.ts
@@ -0,0 +1,15 @@
+import { axiosInstance } from "@apis/axiosInstance";
+
+import { UpdateArticleRequestBody } from "@type/apis/articles/UpdateArticle";
+import { Article } from "@type/models/Article";
+
+import { DOMAIN } from "@constants/api";
+
+export const updateArticle = async (article: UpdateArticleRequestBody) => {
+  const { data } = await axiosInstance.put<Article>(
+    DOMAIN.UPDATE_ARTICLE,
+    article
+  );
+
+  return data;
+};
