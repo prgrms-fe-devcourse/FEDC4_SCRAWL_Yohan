0444333f54221524085cb8687c1ca8b873f7bdc8
doggopawer <doggopawer@gmail.com>
2023-09-21 16:59:15 +0900

    feat : HomePage 제작 - getArticles API 함수 제작 - useArticlesQuery 쿼리 제작 - 모든 글 무한 스크롤링

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,56 @@
-import { useLoggedIn } from "@hooks/useLoggedIn";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+import ErrorBoundary from "@components/_errorBoundaries/ErrorBoundary";
+import Flex from "@components/atoms/Flex";
+import Text from "@components/atoms/Text";
+import Card from "@components/organisms/Card/Card";
+
+import { useArticlesQuery } from "@hooks/api/useArticlesQuery";
+import { useIntersectionObserver } from "@hooks/useIntersectionObserver";
+
+import {
+  cardListStyle,
+  channelPageHeaderStyle,
+  channelPageOuterStyle
+} from "./ChannelPage/ChannelPage.styles";
+
+const PAGE_LIMIT = 10;
 
 const HomePage = () => {
-  const { isLoggedIn } = useLoggedIn();
+  const lastElementRef = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(lastElementRef, { threshold: 1 });
+  const [page, setPage] = useState(1);
 
-  return <div>home loggedIn: {JSON.stringify(isLoggedIn)}</div>;
+  const { articles } = useArticlesQuery();
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      setPage((prevState) => prevState + 1);
+    }
+  }, [entry?.isIntersecting]);
+
+  return (
+    <Flex direction="column" css={channelPageOuterStyle}>
+      <Flex align="center" css={channelPageHeaderStyle}>
+        <Text size={32} strong={true}>
+          최신글
+        </Text>
+      </Flex>
+      <Flex gap={30} css={cardListStyle}>
+        {articles
+          ?.filter((_, index) => {
+            return index < page * PAGE_LIMIT;
+          })
+          .map((article) => (
+            <Fragment key={article._id}>
+              <ErrorBoundary fallback={null}>
+                <Card article={article} width={300} />
+              </ErrorBoundary>
+            </Fragment>
+          ))}
+      </Flex>
+      <div ref={lastElementRef} />
+    </Flex>
+  );
 };
 
 export default HomePage;
