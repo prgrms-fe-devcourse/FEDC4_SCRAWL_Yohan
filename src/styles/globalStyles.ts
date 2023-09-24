700044f1753135b70fcf566dcec1b5865cbfb6fb
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-24 11:49:11 +0900

    design: 스크롤바 스타일 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -14,6 +14,22 @@ export const getGlobalStyles = (theme: Theme) => {
     }
     body {
       margin: 0;
+      ::-webkit-scrollbar-thumb {
+        background-color: ${theme.type === "LIGHT" ? "lightgray" : "gray"};
+      }
+    }
+
+    ::-webkit-scrollbar {
+      width: 0.5em;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 0.25em;
+    }
+    :hover::-webkit-scrollbar-thumb {
+      background-color: ${theme.type === "LIGHT" ? "lightgray" : "gray"};
     }
   `;
 };
