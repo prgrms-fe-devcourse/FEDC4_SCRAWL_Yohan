cf18770660e8486742dc8a54cc0b3f0f636979ef
AhnHyesu <linim@naver.com>
2023-09-25 13:51:10 +0900

    feat: 사이드바 토글을 위한 sidebar context 추가

---
+++ src/contexts/Sidebar/sidebar.context.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+
+type SidebarContextType = {
+  sidebarAppear: boolean;
+  setSidebarAppear: (value: boolean) => void;
+};
+
+export const SidebarContext = createContext<SidebarContextType | undefined>(
+  undefined
+);
+type SidebarProviderProps = {
+  children: React.ReactNode;
+};
+const SidebarProvider = ({ children }: SidebarProviderProps) => {
+  const [sidebarAppear, setSidebarAppear] = useState(true);
+
+  return (
+    <SidebarContext.Provider value={{ sidebarAppear, setSidebarAppear }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export default SidebarProvider;
