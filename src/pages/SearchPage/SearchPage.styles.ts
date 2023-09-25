11a6cf47f79ad8e98f03d79a28260cee06ccf317
dlwl98 <ff981113@hanyang.ac.kr>
2023-09-25 23:38:20 +0900

    fix: 검색 페이지 최대너비 지정

--- src/pages/SearchPage/SearchPage.styles.ts
+++ src/pages/SearchPage/SearchPage.styles.ts
@@ -1,7 +1,10 @@
 import { css } from "@emotion/react";
 
+import { MAX_WIDTH } from "@constants/width";
+
 export const searchPageOuterStyle = css`
   margin: 20px 0;
+  max-width: ${MAX_WIDTH.lg}px;
 `;
 
 export const searchPageHeaderStyle = css`
