aaae3e0102f9e44b50c154408db416d34097d70c
doggopawer <doggopawer@gmail.com>
2023-09-26 10:55:32 +0900

    feat : 썸네일 업로드 형식 수정 - CORS 에러로 인해 파일 업로드 방식 사용

--- src/components/organisms/ArticleWrite/ThumnailChooseModal.tsx
+++ src/components/organisms/ArticleWrite/ThumnailChooseModal.tsx
@@ -1,45 +1,30 @@
+import { ChangeEventHandler, MouseEventHandler } from "react";
+
 import { css } from "@emotion/react";
 
 import Button from "@components/atoms/Button";
 import Flex from "@components/atoms/Flex";
+import Image from "@components/atoms/Image";
 import Text from "@components/atoms/Text";
 
 import { useThemeStore } from "@stores/theme.store";
 
+import noImage from "@assets/svg/noImage.svg";
+
 type ThumnailChooseModalProps = {
-  thumnailUrl: string;
-  imageUrls: string[];
-  onImageClick: (imageUrl: string) => void;
-  onButtonClick: () => void;
+  imageFile: File | null;
+  imageUrl: string;
+  onImageChange: ChangeEventHandler<HTMLInputElement>;
+  onButtonClick: MouseEventHandler<HTMLButtonElement>;
 };
 
 const ThumnailChooseModal = ({
-  thumnailUrl,
-  imageUrls,
-  onImageClick,
+  imageFile,
+  imageUrl,
+  onImageChange,
   onButtonClick
 }: ThumnailChooseModalProps) => {
   const theme = useThemeStore((state) => state.theme);
-
-  if (imageUrls.length === 0) {
-    return (
-      <Flex
-        direction="column"
-        justify="center"
-        align="center"
-        gap={10}
-        css={css`
-          box-sizing: border-box;
-          padding: 24px;
-          width: 320px;
-          height: 400px;
-          border-radius: 8px;
-          background-color: ${theme.BACKGROUND100};
-        `}>
-        <Button onClick={onButtonClick}>기본 썸네일로 글 생성</Button>
-      </Flex>
-    );
-  }
 
   return (
     <Flex
@@ -59,19 +44,29 @@ const ThumnailChooseModal = ({
         썸네일을 선택해주세요.
       </Text>
       <Flex justify="center" align="center" gap={20}>
-        {imageUrls.map((imageUrl) => (
-          <img
+        <label htmlFor="uploadImageFile">
+          <Image
             css={css`
-              width: 30%;
-              border: 3px solid
-                ${thumnailUrl === imageUrl ? theme.PRIMARY : theme.BORDER100};
+              cursor: pointer;
+              border: 1px solid ${theme.BORDER100};
             `}
-            onClick={() => onImageClick(imageUrl)}
-            src={imageUrl}
-          />
-        ))}
+            src={imageUrl || noImage}
+            width={150}
+            height={150}
+            alt={"이미지를 불러올 수 없습니다."}
+            mode="contain"></Image>
+        </label>
+        <input
+          onChange={onImageChange}
+          type="file"
+          id="uploadImageFile"
+          accept="image/*"
+          hidden={true}
+        />
       </Flex>
-      <Button onClick={onButtonClick}>현재 썸네일로 글 생성</Button>
+      <Button onClick={onButtonClick}>
+        {imageFile ? "현재" : "기본"} 썸네일로 글 생성
+      </Button>
     </Flex>
   );
 };
