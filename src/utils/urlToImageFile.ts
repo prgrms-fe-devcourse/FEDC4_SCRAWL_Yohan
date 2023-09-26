960d63e7c6de082131839d871179df152e979ffb
doggopawer <doggopawer@gmail.com>
2023-09-26 10:56:03 +0900

    remove : 필요 없는 유틸 함수 제거 - extractImageUrls 삭제 - urlToImageFile 삭제

--- src/utils/urlToImageFile.ts
+++
@@ -1,7 +0,0 @@
-import { PROXY_URL } from "@constants/api";
-
-export const urlToImageFile = (url: string, fileName: string) => {
-  return fetch(PROXY_URL + url)
-    .then((response) => response.arrayBuffer())
-    .then((buffer) => new File([buffer], fileName, { type: "image/jpeg" }));
-};
